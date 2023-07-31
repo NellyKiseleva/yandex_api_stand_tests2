56716916ef05201e978af8388a08f95286827a9d
Nelly <nellylazy@yandex.ru>
2023-07-31 09:44:17 +0300

    My first commit

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,6 +7,14 @@ def post_new_user(body):
                          json=body,
                          headers=data.headers)
 
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+response = get_users_table()
+print(response.status_code)
+
 def post_products_kits(products_ids):
     return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
                          headers=data.headers)
